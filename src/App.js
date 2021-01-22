2a034e3b730e7d784864b15366dab2bfff6788ca
Tung Teng <thetungctn@gmail.com>
2021-01-23 01:08:16 +0700

    Setup xstyled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import {
+  defaultTheme,
+  ThemeProvider,
+  Preflight,
+} from "@xstyled/styled-components";
+import React from "react";
+
+import Button from "./atoms/Button";
+
+const theme = {
+  ...defaultTheme,
+  // Customize theme here
+};
+
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Preflight />
+      <Button>Click me</Button>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/atoms/Button.js
@@ -0,0 +1,5 @@
+import { x } from "@xstyled/styled-components";
+
+const Button = (props) => <x.button bg="blue-500" {...props} />;
+
+export default Button;
