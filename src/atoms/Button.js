38617d8d7b5b0fbee7c9b69232cb40fd42331f24
Tung Teng <thetungctn@gmail.com>
2021-01-27 21:15:11 +0700

    Button component

--- src/atoms/Button.js
+++ src/atoms/Button.js
@@ -1,6 +1,105 @@
-import { x } from "@xstyled/styled-components";
-import React from 'react';
+import { x } from '@xstyled/styled-components'
+import React from 'react'
 
-const Button = (props) => <x.button bg="blue-500" {...props} />;
+import Icon from './Icon'
 
-export default Button;
+const Button = ({
+  color: colorProp = 'default',
+  variant,
+  size = 'md',
+  startIcon,
+  endIcon,
+  children,
+  ...props
+}) => {
+  const colors = {
+    default: {
+      bg: 'true-gray-200',
+      hoverBg: 'true-gray-400',
+      text: 'black',
+    },
+    primary: {
+      bg: 'blue-500',
+      hoverBg: 'blue-600',
+    },
+    secondary: {
+      bg: 'blue-gray-500',
+      hoverBg: 'blue-gray-600',
+    },
+    success: {
+      bg: 'green-500',
+      hoverBg: 'green-600',
+    },
+    warning: {
+      bg: 'yellow-500',
+      hoverBg: 'yellow-600',
+    },
+    danger: {
+      bg: 'red-500',
+      hoverBg: 'red-600',
+    },
+  }[colorProp]
+
+  const baseProps = {
+    borderRadius: 'md',
+    bg: colors.bg,
+    hoverBg: colors.hoverBg,
+    color: colors.text || 'white',
+    borderWidth: true,
+    borderColor: 'transparent',
+    boxShadow: 'md',
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    verticalAlign: 'middle',
+    disabledPointerEvents: 'none',
+    disabledOpacity: 0.75,
+  }
+
+  const sizeProps = {
+    sm: {
+      py: 2,
+      px: 3,
+    },
+    md: {
+      py: 3,
+      px: 4,
+    },
+    lg: {
+      py: 4,
+      px: 5,
+    },
+  }[size]
+
+  const variantProps =
+    {
+      outline: {
+        bg: 'white',
+        hoverBg: colors.bg,
+        disabledBg: 'white',
+        borderColor: colors.bg,
+        color: colors.text || colors.bg,
+        hoverColor: colors.text || 'white',
+      },
+      text: {
+        borderWidth: 0,
+        px: 0,
+        py: 0,
+        boxShadow: 'none',
+        color: colors.text || colors.bg,
+        hoverColor: colors.text || colors.hoverBg,
+        bg: 'transparent',
+        hoverBg: 'transparent',
+      },
+    }[variant] || {}
+
+  return (
+    <x.button {...baseProps} {...sizeProps} {...variantProps} {...props}>
+      {startIcon && <Icon name={startIcon} mr={2} />}
+      {children}
+      {endIcon && <Icon name={endIcon} ml={2} />}
+    </x.button>
+  )
+}
+
+export default Button
--- src/atoms/Icon.js
+++ src/atoms/Icon.js
@@ -1,21 +1,20 @@
 import * as MdIcons from 'react-icons/md'
-import React from 'react';
+import React from 'react'
 import styled from '@xstyled/styled-components'
 
-const capitalize = (name) => {
+const capitalize = name => {
   return name
     .split('_')
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
     .join('')
 }
 
 const Svg = styled.svgBox``
 
 const Icon = ({ name, ...props }) => {
-  console.log(`Md${capitalize(name)}`)
   const Component = MdIcons[`Md${capitalize(name)}`]
 
-  return <Svg as={Component} {...props} />
+  return <Svg as={Component} display="inline" {...props} />
 }
 
 export default Icon
